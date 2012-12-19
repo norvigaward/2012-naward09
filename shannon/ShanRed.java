9cec0b1afb260598bed1c939ea275715727bcc8a
aerkenbrand <aerkenbrand@gmail.com>
2012-12-19 19:20:33 +0100

    Removed obsolete class

--- shannon/ShanRed.java
+++
@@ -1,7 +0,0 @@
-package shannon;
-
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
-
-public class ShanRed extends LongSumReducer<Text> { //extends Reducer<Text, LongWritable, Text, LongWritable> {
-}
