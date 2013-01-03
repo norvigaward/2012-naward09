8b167a2e29a24a63a6638665ea98b7a9fe5f1355
Menno <menno@tammens.net>
2013-01-03 19:50:47 +0100

    CharMapper: private int size -> private static final int SIZE

--- charCount/CharMapper.java
+++ charCount/CharMapper.java
@@ -5,22 +5,22 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CharMapper<KEY> extends Mapper<KEY, Text, Text, LongWritable> {
-	private int size = 8;
+	private static final int SIZE = 8;
 	private static final LongWritable ONE = new LongWritable(1L);
-	private StringBuilder builder = new StringBuilder(size);
+	private StringBuilder builder = new StringBuilder(SIZE);
 	private Text txt = new Text();
-	private char[] cycle = new char[size];
+	private char[] cycle = new char[SIZE];
 
 	public void map(KEY key, Text val, Context context) {
 		String line = val.toString();
 
 		// ~ Check if this line is worth spending effort on
-		if (line.length() >= this.size) {
+		if (line.length() >= SIZE) {
 			int i = 0;
 
 			// ~ Fill the buffer
 			int useless = 0;
-			for (int j = 0; (j < this.size + useless) && (j < line.length()); j++) {
+			for (int j = 0; (j < SIZE + useless) && (j < line.length()); j++) {
 				char currChar = Character.toLowerCase(line.charAt(j));
 
 				// Only a-z
@@ -33,14 +33,14 @@ public class CharMapper<KEY> extends Mapper<KEY, Text, Text, LongWritable> {
 			}
 
 			// ~ Start looping through the rest of the string
-			for (int j = (this.size - 1); j < line.length(); j++) {
+			for (int j = (SIZE - 1); j < line.length(); j++) {
 				char currChar = Character.toLowerCase(line.charAt(j));
 				if (isValid(currChar)) {
-					i = i % size;
+					i = i % SIZE;
 					this.cycle[i] = currChar;
 
 					// Build the output from the cycle
-					builder.delete(0, size);
+					builder.delete(0, SIZE);
 					builder.append(this.cycle, i, (this.cycle.length - i));
 					builder.append(this.cycle, 0, i);
 
