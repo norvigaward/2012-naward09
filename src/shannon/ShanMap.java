89563759357f41cffcf77db533ed2a5de0c05617
aerkenbrand <aerkenbrand@gmail.com>
2013-01-04 21:55:03 +0100

    Added a DIFF It keeps track of the amount of different combinations. Basically a linecount on an uncompressed output of the charCount

--- src/shannon/ShanMap.java
+++ src/shannon/ShanMap.java
@@ -7,15 +7,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class ShanMap extends Mapper<Text, Text, Text, DoubleWritable> {
 	private static final Text LOG = new Text("LOG");
 	private static final Text TOTAL = new Text("TOTAL");
+	private static final Text DIFF = new Text("DIFF");
+	private static final DoubleWritable ONE = new DoubleWritable(1);
 
 	public void map(Text key, Text val, Context context) {
 		double count = Double.parseDouble(val.toString());
 		
-		double output = (1.0 / count) * Math.log(count);
+		double output = count * Math.log(count);
 
 		try {
 			context.write(LOG, new DoubleWritable(output));
 			context.write(TOTAL, new DoubleWritable(count));
+			context.write(DIFF, ONE);
 		} catch (Exception e) {
 			System.out.println("Error writing to context");
 		}
